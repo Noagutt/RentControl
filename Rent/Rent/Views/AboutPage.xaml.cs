32c5d063a7095a6a13d31272905353fd0e4ce116
Noa Guttman <guttmannoa@gmail.com>
2018-05-07 22:54:03 +0300

    Add project files.

---
+++ Rent/Rent/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Rent.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
